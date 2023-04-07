32db423a56387cd372b3b7a1da984a6fa0e1f83d
Higor Jugler <higorjugler@hotmail.com>
2023-04-07 09:56:33 -0300

    primeira activity criada

---
+++ app/src/main/java/com/example/phonebook/ui/activity/ListaProdutoActivity.kt
@@ -0,0 +1,14 @@
+package com.example.phonebook.ui.activity
+
+import android.app.Activity
+import android.os.Bundle
+import com.example.phonebook.R
+
+class ListaProdutoActivity : Activity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_lista_produtos)
+    }
+
+}
