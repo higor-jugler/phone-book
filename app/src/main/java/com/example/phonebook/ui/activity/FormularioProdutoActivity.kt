d121644f3cc5cf52b04cf54f86f1e61f0f8f6e40
Higor Jugler <higorjugler@hotmail.com>
2023-04-07 14:29:38 -0300

    Adicionado a função p botão add

---
+++ app/src/main/java/com/example/phonebook/ui/activity/FormularioProdutoActivity.kt
@@ -0,0 +1,16 @@
+package com.example.phonebook.ui.activity
+
+import android.app.Activity
+import android.content.Intent
+import android.os.Bundle
+import android.os.PersistableBundle
+import com.example.phonebook.R
+
+class FormularioProdutoActivity : Activity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_formulario_produto)
+    }
+
+}
--- app/src/main/java/com/example/phonebook/ui/activity/ListaProdutoActivity.kt
+++ app/src/main/java/com/example/phonebook/ui/activity/ListaProdutoActivity.kt
@@ -1,7 +1,9 @@
 package com.example.phonebook.ui.activity
 
 import android.app.Activity
+import android.content.Intent
 import android.os.Bundle
+import android.view.View
 import com.example.phonebook.R
 
 class ListaProdutoActivity : Activity() {
@@ -9,6 +11,19 @@ class ListaProdutoActivity : Activity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_lista_produtos)
+        configFab()
+    }
+
+    private fun configFab() {
+        val fab: View = findViewById(R.id.activity_lista_botao_add)
+        fab.setOnClickListener { it ->
+            configFormularioProduto()
+        }
+    }
+
+    private fun configFormularioProduto() {
+        val intent = Intent(this, FormularioProdutoActivity::class.java)
+        startActivity(intent)
     }
 
 }
