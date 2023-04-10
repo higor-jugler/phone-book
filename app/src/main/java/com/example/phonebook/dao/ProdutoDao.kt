f3f4be2b1fcfc618bd675fd84ab582f64920ec35
Higor Jugler <higorjugler@hotmail.com>
2023-04-10 08:17:21 -0300

    Criada a class p data access object

---
+++ app/src/main/java/com/example/phonebook/dao/ProdutoDao.kt
@@ -0,0 +1,19 @@
+package com.example.phonebook.dao
+
+import com.example.phonebook.model.Produto
+
+class ProdutoDao {
+
+    fun adiciona(produto: Produto) {
+        produtos.add(produto)
+    }
+
+    fun buscaTodos(): List<Produto> {
+        return produtos.toList()
+    }
+
+    companion object {
+        private val produtos = mutableListOf<Produto>()
+    }
+
+}
